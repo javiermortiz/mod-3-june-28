bf2005fb0f8e86e1292f648e1b72ee1b0ca6a567
Javier Ortiz <javier.ortiz989@gmail.com>
2021-08-24 08:43:23 -0700

    tuesday demo and monday long practice solution

--- week-9/tuesday/element-selection-and-manipulation-demo/index.js
+++ week-9/tuesday/element-selection-and-manipulation-demo/index.js
@@ -1,4 +1,28 @@
 window.onload = () => {
+    const button = document.getElementById("click-me")
+    console.log(button);
+    button.addEventListener("click", ev => {
+        const ball = document.getElementById("ball");
+        console.log(ball);
+        ball.classList.add("red-ball");
 
+        // const instructor = document.querySelector(".instructor");
+        // console.log(instructor);
+        // instructor.style.color = "red";
 
+        const instructors = document.querySelectorAll(".instructor");
+        console.log(instructors);
+        let colors = ["red", "green", "blue"]
+        instructors.forEach((instructor, i) => {
+            instructor.classList.add("red")
+        });
+
+        const playground = document.getElementById("playground");
+        playground.innerHTML += `
+            <h2 class='red'>Hope you enjoyed the demo!</h2>
+            <img
+                src='https://p4.wallpaperbetter.com/wallpaper/70/625/78/animal-baby-cute-dog-wallpaper-preview.jpg'
+            >
+        `;
+    })
 }
