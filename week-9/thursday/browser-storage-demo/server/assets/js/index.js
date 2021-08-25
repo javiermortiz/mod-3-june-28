c5bb900ad703d57562da422156ebff8576cd2ffc
Javier Ortiz <javier.ortiz989@gmail.com>
2021-08-25 16:02:44 -0700

    catsagram day 2

---
+++ week-9/thursday/browser-storage-demo/server/assets/js/index.js
@@ -0,0 +1,33 @@
+window.addEventListener("DOMContentLoaded", ev => {
+
+});
+
+function lightMode() {
+    const body = document.querySelector("body");
+    const h1 = document.querySelector("h1");
+    const a = document.querySelectorAll("a");
+    const ul = document.querySelector("ul");
+    const button = document.querySelector("button");
+    const li = document.querySelectorAll("li");
+    body.className = "";
+    h1.className = "";
+    a.forEach(tag => tag.className = "");
+    ul.className = "";
+    button.className = "";
+    li.forEach(tag => tag.className = "");
+}
+
+function darkMode() {
+    const body = document.querySelector("body");
+    const h1 = document.querySelector("h1");
+    const a = document.querySelectorAll("a");
+    const ul = document.querySelector("ul");
+    const button = document.querySelector("button");
+    const li = document.querySelectorAll("li");
+    body.className = "dark";
+    h1.className = "dark";
+    a.forEach(tag => tag.className = "dark");
+    ul.className = "dark";
+    button.className = "dark";
+    li.forEach(tag => tag.className = "dark");
+}
---
+++ week-9/wednesday/solutions/battleship/starter/assets/js/index.js
@@ -0,0 +1,5 @@
+// Imports
+
+window.onload = () => {
+    
+}
